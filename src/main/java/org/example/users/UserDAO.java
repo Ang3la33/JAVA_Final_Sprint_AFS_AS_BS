b21ce2e0b5fce495caf0f2274c2ed43bfca68465
Adam-S988 <adam.stevenson@keyin.com>
2024-12-02 17:49:16 -0330

    Followed most of the first Maven video. Started the Connection, and a User Class.

--- src/main/java/org/example/users/UserDAO.java
+++ src/main/java/org/example/users/UserDAO.java
@@ -1,4 +1,39 @@
 package org.example.users;
 
+import org.example.database.DatabaseConnection;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class UserDAO {
+    public void getCars(){
+        ResultSet rs = null;
+        String sql = "SELECT * FROM USERS";
+
+        try(Connection conn = DatabaseConnection.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)){
+
+        rs = pstmt.executeQuery();
+        while(rs.next()) {
+            int user_id = rs.getInt("user_id");
+            String user_username = rs.getString("user_username");
+            String user_password = rs.getString("user_password");
+            String user_role = rs.getString("user_role");
+
+            System.out.println("user_id: " + user_id);
+            System.out.println("user_username: " + user_username);
+            System.out.println("user_password: " + user_password);
+            System.out.println("user_role: " + user_role);
+            System.out.println("------------------------");
+        }
+
+//        pstmt.setString(1, users.getUsers_username());
+//        pstmt.setString(2, users.getUsers_password());
+//        pstmt.setString(3, users.getUsers_role());
+//        pstmt.executeUpdate();
+
+            //Pickup video at ~30 minutes
+//    }
+
+        }
 }
--- src/main/java/org/example/users/Users.java
+++ src/main/java/org/example/users/Users.java
@@ -1,4 +1,54 @@
 package org.example.users;
 
 public class Users {
+
+    private int user_id;
+    private String user_username;
+    private String user_password;
+    private String user_role;
+
+    public Users(String user_username, String user_password, String user_role) {
+        this.user_username = user_username;
+        this.user_password = user_password;
+        this.user_role = user_role;
+    }
+
+    public Users(int user_id, String user_username, String user_password, String user_role) {
+        this.user_id = user_id;
+        this.user_username = user_username;
+        this.user_password = user_password;
+        this.user_role = user_role;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUser_username() {
+        return user_username;
+    }
+
+    public void setUser_username(String user_username) {
+        this.user_username = user_username;
+    }
+
+    public String getUser_password() {
+        return user_password;
+    }
+
+    public void setUser_password(String user_password) {
+        this.user_password = user_password;
+    }
+
+    public String getUser_role() {
+        return user_role;
+    }
+
+    public void setUser_role(String user_role) {
+        this.user_role = user_role;
+    }
 }
